9322d8994c2c41f26b11b89035aa424000510ed5
rohandevtown <rohankinnal@devtown.in>
2024-06-07 18:26:26 +0530

    d2

--- Readme.md
+++ Readme.md
@@ -3,3 +3,17 @@
 > > node server.js
 > > npm start
 > > npm run dev
+
+## HTTP Methods:
+
+    >> GET: Getting the data
+    >> POST: Sending data
+    >> DELETE: Deleting the data
+    >> PATCH: Update certain fields
+    >> PUT: Full Update
+
+Browser (JSON Data) --------> Chunks/Data Bits ---------> Server
+
+> > Postman
+> > Thunderclient
+> > Insomina
--- server.js
+++ server.js
@@ -2,9 +2,39 @@ const http = require("http");
 
 const port = 8081;
 
+const toDoList = ["rohan", "rohit", "anil", "anup"];
+
 http.createServer((req, res)=> {
-    res.writeHead(200, {"Content-Type": "text/html"});
-    res.write("<h1>Hello rohan kinnal 12345678910!</h1>");
+    const {method, url} = req;
+    // console.log(method, url)
+
+    if(url === "/todos"){
+        if(method === "GET"){
+            res.writeHead(200);
+            res.write(toDoList.toString())
+        }
+       
+        else if(method === "POST"){
+            let body = "";
+            req.on('error',(err)=>{
+                console.error(err)
+            }).on('data', (chunk) => {
+                body += chunk;
+                console.log("chunk: ", chunk);
+            }).on('end', () => {
+                body = JSON.parse(body)
+                console.log("body: ", body);
+                let newToDo = toDoList;
+                newToDo.push(body.item)
+            })
+        }
+        else{
+            res.writeHead(501);
+        }
+    }else if(url === "/"){
+
+    }
+
     res.end();
 })
 .listen(port, () => {
